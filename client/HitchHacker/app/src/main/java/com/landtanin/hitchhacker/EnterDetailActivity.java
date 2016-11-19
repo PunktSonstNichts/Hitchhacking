4e7fb6a83dfe8f9fef7841dd541c03634523a6a4
Tanin Rojanapiansatith <sp_land@hotmail.com>
2016-11-19 21:48:32 +0000

    get the POST work with exactly the same code

--- client/HitchHacker/app/src/main/java/com/landtanin/hitchhacker/EnterDetailActivity.java
+++ client/HitchHacker/app/src/main/java/com/landtanin/hitchhacker/EnterDetailActivity.java
@@ -10,12 +10,20 @@ import android.widget.Toast;
 
 import com.landtanin.hitchhacker.databinding.ActivityEnterDetailBinding;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import okhttp3.FormBody;
 import okhttp3.HttpUrl;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+
+import static com.google.common.net.HttpHeaders.USER_AGENT;
 
 public class EnterDetailActivity extends AppCompatActivity {
 
@@ -38,10 +46,73 @@ public class EnterDetailActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 connectDatabase();
+
+//                new AsyncTask<Void, Void, String>() {
+//
+//                    @Override
+//                    protected String doInBackground(Void... voids) {
+//
+//                        try {
+//                            sendPost();
+//                        } catch (Exception e) {
+//                            e.printStackTrace();
+//                        }
+//                        return null;
+//                    }
+//
+//
+//                }.execute();
+
                 Toast.makeText(EnterDetailActivity.this, "onClick", Toast.LENGTH_SHORT).show();
 
             }
         });
+
+    }
+
+    // HTTP POST request
+    private void sendPost() throws Exception {
+
+        String url = "http://10.232.29.59/Hitchhacking/Hitchhacking/server/accessUsers.php";
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+        //add request header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        String urlParameters = "anything";
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+//        System.out.println("\nSending 'POST' request to URL : " + url);
+//        System.out.println("Post parameters : " + urlParameters);
+//        System.out.println("Response Code : " + responseCode);
+
+        Log.d("1","Sending 'POST' request to URL : " + url);
+        Log.d("2","Post parameters : " + urlParameters);
+        Log.d("3","Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+//        System.out.println(response.toString());
+        Log.d("RESULT", response.toString());
 
     }
 
