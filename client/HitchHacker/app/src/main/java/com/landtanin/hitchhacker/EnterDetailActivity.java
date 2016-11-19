c1bc063019e4ec886ac0d2c721718e40901f8d90
Tanin Rojanapiansatith <sp_land@hotmail.com>
2016-11-19 23:07:35 +0000

    start the Drive or HitchHike Activity

--- client/HitchHacker/app/src/main/java/com/landtanin/hitchhacker/EnterDetailActivity.java
+++ client/HitchHacker/app/src/main/java/com/landtanin/hitchhacker/EnterDetailActivity.java
@@ -1,5 +1,6 @@
 package com.landtanin.hitchhacker;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -10,20 +11,12 @@ import android.widget.Toast;
 
 import com.landtanin.hitchhacker.databinding.ActivityEnterDetailBinding;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
 
 import okhttp3.FormBody;
 import okhttp3.HttpUrl;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-
-import static com.google.common.net.HttpHeaders.USER_AGENT;
 
 public class EnterDetailActivity extends AppCompatActivity {
 
@@ -31,7 +24,8 @@ public class EnterDetailActivity extends AppCompatActivity {
 
     private String strUserName, strPassWord;
 
-    private String resultServer;
+    private String resultServer, strApiKey = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,74 +41,62 @@ public class EnterDetailActivity extends AppCompatActivity {
 
                 connectDatabase();
 
-//                new AsyncTask<Void, Void, String>() {
-//
-//                    @Override
-//                    protected String doInBackground(Void... voids) {
-//
-//                        try {
-//                            sendPost();
-//                        } catch (Exception e) {
-//                            e.printStackTrace();
-//                        }
-//                        return null;
-//                    }
-//
-//
-//                }.execute();
-
                 Toast.makeText(EnterDetailActivity.this, "onClick", Toast.LENGTH_SHORT).show();
+
+                Intent objIntent = new Intent(EnterDetailActivity.this, driverOrHitchActivity.class);
+                objIntent.putExtra("api_key", strApiKey);
+                startActivity(objIntent);
 
             }
         });
 
     }
 
-    // HTTP POST request
-    private void sendPost() throws Exception {
-
-        String url = "http://10.232.29.59/Hitchhacking/Hitchhacking/server/accessUsers.php";
-        URL obj = new URL(url);
-        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
-
-        //add request header
-        con.setRequestMethod("POST");
-        con.setRequestProperty("User-Agent", USER_AGENT);
-        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-
-        String urlParameters = "anything";
-
-        // Send post request
-        con.setDoOutput(true);
-        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-        wr.writeBytes(urlParameters);
-        wr.flush();
-        wr.close();
-
-        int responseCode = con.getResponseCode();
-//        System.out.println("\nSending 'POST' request to URL : " + url);
-//        System.out.println("Post parameters : " + urlParameters);
-//        System.out.println("Response Code : " + responseCode);
-
-        Log.d("1","Sending 'POST' request to URL : " + url);
-        Log.d("2","Post parameters : " + urlParameters);
-        Log.d("3","Response Code : " + responseCode);
-
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-
-        //print result
-//        System.out.println(response.toString());
-        Log.d("RESULT", response.toString());
-
-    }
+//    // HTTP POST request
+//    private void sendPost() throws Exception {
+//
+//        String url = "http://10.232.29.59/Hitchhacking/Hitchhacking/server/accessUsers.php";
+//        URL obj = new URL(url);
+//        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+//
+//        //add request header
+//        con.setRequestMethod("POST");
+//        con.setRequestProperty("User-Agent", USER_AGENT);
+//        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+//
+//        String urlParameters = "anything";
+//
+//        // Send post request
+//        con.setDoOutput(true);
+//        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+//        wr.writeBytes(urlParameters);
+//        wr.flush();
+//        wr.close();
+//
+//        int responseCode = con.getResponseCode();
+////        System.out.println("\nSending 'POST' request to URL : " + url);
+////        System.out.println("Post parameters : " + urlParameters);
+////        System.out.println("Response Code : " + responseCode);
+//
+//        Log.d("1","Sending 'POST' request to URL : " + url);
+//        Log.d("2","Post parameters : " + urlParameters);
+//        Log.d("3","Response Code : " + responseCode);
+//
+//        BufferedReader in = new BufferedReader(
+//                new InputStreamReader(con.getInputStream()));
+//        String inputLine;
+//        StringBuffer response = new StringBuffer();
+//
+//        while ((inputLine = in.readLine()) != null) {
+//            response.append(inputLine);
+//        }
+//        in.close();
+//
+//        //print result
+////        System.out.println(response.toString());
+//        Log.d("RESULT", response.toString());
+//
+//    }
 
     private void connectDatabase() {
 
@@ -156,6 +138,16 @@ public class EnterDetailActivity extends AppCompatActivity {
 
 
                 Log.d("CONNECTDATABASE", String.valueOf(response));
+
+                try {
+                    resultServer = response.body().string();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                //TODO: get the API key
+                Log.d("RESULTSERVER", resultServer);
+                Toast.makeText(EnterDetailActivity.this, resultServer, Toast.LENGTH_SHORT).show();
 
                 return null;
             }
